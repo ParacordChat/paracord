914c94e1910783d874acab402b9927ca8eb76e7b
Jacob Trock <jtbosshku@gmail.com>
2023-10-27 10:08:26 -0400

    iterate build.sh

--- nativeApp/build.sh
+++ nativeApp/build.sh
@@ -1,27 +1,36 @@
 rm -rf ./build/*
 
-nativefier 'https://paracordchat.com' ./build  -a "universal" --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.icns -p "mac" --verbose
+globalenv="{\"paracordDesktop\":\"true\"}"
 
-nativefier 'https://paracordchat.com' ./build  -a "x64" --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.png -p "linux" --verbose
-nativefier 'https://paracordchat.com' ./build  -a "arm64" --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.png -p "linux" --verbose
+nativefier 'https://paracordchat.com' ./build  -a "x64" --portable --process-envs $globalenv --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.icns --app-copyright 'Jacob Trock' -p "mac" --verbose
+nativefier 'https://paracordchat.com' ./build  -a "arm64" --portable --process-envs $globalenv --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.icns --app-copyright 'Jacob Trock' -p "mac" --verbose
 
-nativefier 'https://paracordchat.com' ./build  -a "x64" --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.ico -p "windows" --verbose
+nativefier 'https://paracordchat.com' ./build  -a "x64" --portable --process-envs $globalenv --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.png -p "linux" --verbose
+nativefier 'https://paracordchat.com' ./build  -a "arm64" --portable --process-envs $globalenv --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.png -p "linux" --verbose
 
-touch ./build/sums.nfo
+win32metadatastring="{\"CompanyName\":\"Edisys\"}"
+nativefier 'https://paracordchat.com' ./build  -a "x64" --portable --process-envs $globalenv --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.ico --app-copyright 'Jacob Trock' -p "windows" --verbose --win32metadata $win32metadatastring
 
-echo "Sums:" >> ./build/sums.nfo
+cd build
 
-for folder in ./build/*; do
+mv Paracord-darwin-x64/Paracord.app ./Paracord-mac-intel.app
+mv Paracord-darwin-arm64/Paracord.app ./Paracord-mac-m1.app
+rm -rf Paracord-darwin-*
+
+touch sums.nfo
+echo "Sums:" >> sums.nfo
+
+for folder in ./*; do
     # hash folder
     hash=$(echo "$folder" | md5sum | cut -d' ' -f1)
 
     #compress and add comment with hash
-    zip -r "./build/$(basename "$folder").zip" "$folder"
+    zip -r "./$(basename "$folder").zip" "$folder"
 
     # add hash to sums.nfo
-    echo " - $folder - $hash" >> ./build/sums.nfo
+    echo " - $folder - $hash" >> sums.nfo
 
     rm -rf "$folder"
 done
 
-
+cd ..

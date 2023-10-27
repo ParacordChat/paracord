6e1b1bf63ec3ce6be56eca770938c898ebf19f55
Jacob Trock <jtbosshku@gmail.com>
2023-10-27 00:54:48 -0400

    fix build.sh

--- nativeApp/build.sh
+++ nativeApp/build.sh
@@ -7,8 +7,21 @@ nativefier 'https://paracordchat.com' ./build  -a "arm64" --disable-context-menu
 
 nativefier 'https://paracordchat.com' ./build  -a "x64" --disable-context-menu --disable-dev-tools --min-width 400 --min-height 600 -i ./icons/logo.ico -p "windows" --verbose
 
+touch ./build/sums.nfo
+
+echo "Sums:" >> ./build/sums.nfo
+
 for folder in ./build/*; do
-    tar -czvf "./build/$(basename "$folder").tar.gz" "$folder"
+    # hash folder
+    hash=$(echo "$folder" | md5sum | cut -d' ' -f1)
+
+    #compress and add comment with hash
+    zip -r "./build/$(basename "$folder").zip" "$folder"
+
+    # add hash to sums.nfo
+    echo " - $folder - $hash" >> ./build/sums.nfo
+
     rm -rf "$folder"
 done
+
 

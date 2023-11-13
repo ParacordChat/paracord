f88834437d4170b74db21c29753c048f41f189f5
Jacob Trock <jtbosshku@gmail.com>
2023-11-13 09:33:30 -0500

    add upsell

--- src/views/createView/RoomCreator.tsx
+++ src/views/createView/RoomCreator.tsx
@@ -7,7 +7,7 @@ import {
 	Text,
 	TextInput
 } from "grommet";
-import { Login, Organization, Risk } from "grommet-icons";
+import { Down, Login, Organization, Risk } from "grommet-icons";
 import { route } from "preact-router";
 import { useRef, useState } from "preact/hooks";
 import packageJson from "../../../package.json";
@@ -23,33 +23,82 @@ export function RoomCreator() {
 		<>
 			<GenericHeader>
 				<Box
-					direction="column"
-					style={{ textAlign: "center", paddingBottom: "3em" }}
+					background={{
+						image: "url(./distbg.svg)",
+						size: "cover",
+						position: "center"
+					}}
+					style={{ height: "100vh" }}
 				>
-					<Text size="large" color="#FFFC00">
+					<Box
+						direction="column"
+						style={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+							height: "100vh",
+							textAlign: "center",
+							paddingBottom: "3em"
+						}}
+					>
+						<Text size="large" color="#FFFC00" style={{ background:"black" }}>
             The ghost keeps your messages.{" "}
-						<a href="https://www.nytimes.com/2014/05/09/technology/snapchat-reaches-settlement-with-federal-trade-commission.html?_r=0">
+							<a href="https://www.nytimes.com/2014/05/09/technology/snapchat-reaches-settlement-with-federal-trade-commission.html?_r=0">
               Forever
-						</a>
+							</a>
             .
-					</Text>
-					<Text size="large" color="#7289da">
+						</Text>
+						<Text size="large" color="#7289da" style={{ background:"black" }}>
             Same goes for a certain purple...{" "}
-						<a href="https://www.reddit.com/r/privacy/comments/jy14qi/psa_discord_lies_about_removing_deleted_files/">
+							<a href="https://www.reddit.com/r/privacy/comments/jy14qi/psa_discord_lies_about_removing_deleted_files/">
               creature
-						</a>
+							</a>
             .
-					</Text>
-					<Text size="large">
+						</Text>
+						<Text size="large" style={{ background:"black" }}>
             Want privacy? Choose <b style={{ color: "#4bffac" }}>Para</b>
-						<b style={{ color: "#af79ff" }}>cord</b>.
-					</Text>
-					<Text size="small">
-            There in seconds, gone in seconds. Always yours.
-					</Text>
+							<b style={{ color: "#af79ff" }}>cord</b>.
+						</Text>
+						<Text size="small" style={{ background:"black" }}>
+							Send multiple gigabytes, Call and chat without anyone in the middle.
+						</Text>
+
+						<Box direction="row">
+							<Button style={{ 
+								background:"black",
+								border: "1px solid #af79ff",
+								clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 40%)",
+								padding: "1em",
+								paddingBottom: "2em"
+							}} onClick={() => route("/Enterprise")}>
+								<Box direction="column" style={{
+									alignItems: "center",
+									justifyContent: "center"
+								}}>
+								Organizations
+									<Organization />
+								</Box>
+							</Button>
+							<Button pad="large" style={{ 
+								background:"black",
+								border: "1px solid #4bffac",
+								clipPath: "polygon(0 0, 100% 0, 100% 60%, 0 100%)",
+								padding: "1em"
+							}} onClick={()=>document.querySelector("#roomCreate")
+								?.scrollIntoView()}>
+								<Box direction="column" style={{
+									alignItems: "center",
+									justifyContent: "center"
+								}}>
+								People
+									<Down size="large"/>
+								</Box>
+							</Button>
+						</Box>
+					</Box>
 				</Box>
 
-				<Box border={{ color: "brand", size: "large" }} pad="medium">
+				<Box border={{ color: "brand", size: "large" }} pad="medium" id="roomCreate" style={{ marginTop: "50vh", marginBottom: "50vh" }}>
 					<Text size="medium">Create a room:</Text>
 					<Box direction="row">
 						<TextInput
@@ -88,13 +137,7 @@ export function RoomCreator() {
 					</Box>
 					<Button onClick={loadRoom} label="Go" primary />
 				</Box>
-				<Button
-					icon={<Organization />}
-					style={{ color: "#4bffac" }}
-					onClick={() => route("/Enterprise")}
-					label="Learn about Paracord for your business"
-					size="small"
-				/>
+				
 			</GenericHeader>
 
 			<Footer
